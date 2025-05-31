e9de37cae1477172e039aa9e1c211e8397eeaed6
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2025-05-31 20:48:17 +0300

    feat: enhance Docker setup with development and production configurations, add MongoDB connection, and update dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM node:20
+FROM node:20 As base
 
+
+FROM base As development
 WORKDIR /app
-
 COPY package.json ./
-
 RUN npm install 
-
 COPY . .
-
 EXPOSE 3000
-
 CMD ["npm", "run", "start-dev"]
+
+FROM base As production
+WORKDIR /app
+COPY package.json ./
+RUN npm install --only=production
+COPY . .
+EXPOSE 3000
+CMD ["npm", "start"]
--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const DB_USER = "root";
+const DB_PASSWORD = "password";
+const DB_PORT = "27017";
+const DB_HOST = "mongo";
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("Hello, World !!!");
+  res.send("Hello, World !! ");
 });
 
 app.listen(PORT, () => {
